5561b1df80240a11bdc6da5b44741ef944ab745c
清水俊樹 <shimizutoshiki@MBA-3.local>
2019-08-04 10:27:09 +0900

    create alds1 1d

---
+++ ALDS1/1d.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+static const int MAX = 200000;
+int main(){
+    int R[MAX], N;
+    cin >> N;
+    for (int i=0; i<N; i++){
+        cin >> R[i];
+    }
+    int mx = -1000000000;
+    int mn = R[0];
+    for  (int i=1; i<N; i++){
+        mx = max(mx, R[i]-mn);
+        mn = min(mn, R[i]);
+    }
+    cout << mx << endl;
+    return 0;
+}
