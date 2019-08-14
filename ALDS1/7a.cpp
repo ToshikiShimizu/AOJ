3ba7e317d05e3d25d3ff5f486ea78fd3e38149a8
清水俊樹 <shimizutoshiki@MBA-3.local>
2019-08-14 09:47:55 +0900

    7a

--- ALDS1/7a.cpp
+++ ALDS1/7a.cpp
@@ -7,7 +7,31 @@ struct Node{
     int parent;
     int left;
     int right;
+    int depth;
+    string type;
 };
+int getRoot(Node nodes[], int n){
+    for (int i=0;i<n;i++){
+        if(nodes[i].parent == NIL){
+            return i;
+        }
+    }
+}
+void setDepth(Node nodes[], int id){
+    if (nodes[id].parent == NIL){
+        nodes[id].depth=0;
+    }
+    else{
+        nodes[id].depth = nodes[nodes[id].parent].depth + 1;
+    }
+    if (nodes[id].left != NIL){
+        setDepth(nodes, nodes[id].left);
+    }
+    if (nodes[id].right != NIL){
+        setDepth(nodes, nodes[id].right);
+    }
+}
+
 int main(){
     Node nodes[MAX];
     int n;
@@ -20,6 +44,7 @@ int main(){
         nodes[i].parent = NIL;
         nodes[i].left = NIL;
         nodes[i].right = NIL;
+        nodes[i].depth = NIL;
     }
     for(int i=0;i<n;i++){
         cin >> par >> degree;
@@ -34,10 +59,24 @@ int main(){
             nodes[cur].parent = par;
         }
     }
+    int root;
+    root = getRoot(nodes, n);
+    setDepth(nodes, root);
+    for(int i=0;i<n;i++){
+        if (nodes[i].parent == NIL){
+            nodes[i].type = "root";
+        }else if(nodes[i].left != NIL){
+            nodes[i].type = "internal node";
+        }else{
+            nodes[i].type = "leaf";
+        }
+    }
+
     for(int i=0;i<n;i++){
         cout << "node" << " " << i << ": ";
         cout << "parent = " << nodes[i].parent << ", ";
-
+        cout << "depth = " << nodes[i].depth << ", ";
+        cout << nodes[i].type << ", ";
         cout << "[";
         for (int j=nodes[i].left; j!=NIL; j=nodes[j].right){
             if (j!=nodes[i].left){
@@ -46,6 +85,10 @@ int main(){
             cout << j;
         }
         cout << "]";
+
         cout << endl;
     }
 }
+
+
+	
