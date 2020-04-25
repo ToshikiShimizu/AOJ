785ddf21993fcaddcf9b3dfa712370ba56de9fc4
清水俊樹 <shimizutoshiki@MBA-3.local>
2020-04-25 11:41:42 +0900

    fix

--- ALDS1/12b.py
+++ ALDS1/12b.py
@@ -2,7 +2,7 @@ import heapq
 INF = 10**10
 
 V, E, r = map(int,input().split())
-adj = [[] for i in range(E)]
+adj = [[] for i in range(V)]
 
 for e in range(E):
     s, t, d = map(int,input().split())
@@ -14,6 +14,7 @@ dists[r] = 0
 pq = [(0, r)]
 while(pq):
     d, node = heapq.heappop(pq)
+    #print (d,node)
     if (d > dists[node]):#探索の対象にする必要があるか確認
         continue
     for next, cost in adj[node]:
